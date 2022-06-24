175fdfbd2568572880966c27cf2955e82aa1b007
念 <3209752489@qq.com>
2022-06-24 13:50:12 +0800

    WebStorm提示Vue的去全局组件
    Signed-off-by: 念 <3209752489@qq.com>

--- src/interface.ts
+++ src/interface.ts
@@ -1,12 +1,17 @@
 import axios from "./assets/js/axios";
 import env from "@/assets/js/env";
 import functionLibrary from "@/assets/js/functionLibrary";
+import SvgImg from "@/components/common/SvgImg.vue";
 
 declare module '@vue/runtime-core' {
-    interface ComponentCustomProperties {
+    export interface ComponentCustomProperties {
         env: typeof env;
         axios: typeof axios;
         $http: typeof axios;
         functions: functionLibrary;
+    }
+    
+    export interface GlobalComponents {
+        'SvgImg': typeof SvgImg,
     }
 }
